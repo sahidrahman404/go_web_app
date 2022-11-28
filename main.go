2e94ea8f230c2fb738560da54afab220a0c7e7db
Sahid Rahman <sahidrahman404@gmail.com>
2022-11-28 08:05:38 +0700

    add web server

--- main.go
+++ main.go
@@ -22,4 +22,8 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
