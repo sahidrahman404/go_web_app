2aa3947742bdcaf23e9dcd0424bd51158d405abb
Sahid Rahman <sahidrahman404@gmail.com>
2022-11-28 08:20:52 +0700

    restrict the /snippet/create route
    so that the /snippet/create route only responds to HTTP
    requests which use the POST method

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
