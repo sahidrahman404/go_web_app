9c6a01c7e9a29d7d724d69e86580f791e520acbf
Sahid Rahman <sahidrahman404@gmail.com>
2023-03-21 13:22:22 +0700

    add newTemplateCache function for caching template

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -14,3 +14,35 @@ type templateData struct {
 	Snippets    []*models.Snippet
 }
 
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
+
