4a9a063241bc6b79150750fe3ca0af853f209dec
Sahid Rahman <sahidrahman404@gmail.com>
2023-03-21 13:15:39 +0700

    fix database Get fuction

--- pkg/models/postgres/snippets.go
+++ pkg/models/postgres/snippets.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/sahidrahman404/snippetbox/pkg/models"
 )
@@ -24,9 +25,19 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	// stmt := `select * from snippets where id = ?`
+	stmt := `select id, title, content, created, expires from snippets where id = $1`
 
-	return nil, nil
+	s := &models.Snippet{}
+	err := m.DB.QueryRow(stmt, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, nil
 }
 
 func (m *SnippetModel) Latest(id int) ([]*models.Snippet, error) {
